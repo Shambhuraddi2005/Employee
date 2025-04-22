6da532c2126b405a6850e9a505ab2b0c3e5c97b8
Shambhuraddi2005 <Shambhuraddiradder@gail.com>
2025-04-22 15:00:40 +0530

    SHAMBHU

---
+++ Employe.java
@@ -0,0 +1,13 @@
+public class Employe {
+    public static void main(String[] args) {
+        String employeeName = "John Doe";
+        int employeeID = 12345;
+        String department = "Software Development";
+        double salary = 75000.50;
+
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Employee ID: " + employeeID);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: $" + salary);
+    }
+}
